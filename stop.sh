2d0ae3035b1d1be71a8fb38517c09fd7fc977258
ShrutiKhemsheety <133239359+ShrutiKhemsheety@users.noreply.github.com>
2025-11-27 22:19:44 +0530

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-echo "Stopping the service..."
-# Add your stop commands here, e.g., stopping a server or app
-# Example: stopping a Node.js server by killing the process
-# pkill node
-
-# Or stopping a service
-# sudo systemctl stop apache2
-
-echo "Service stopped."
+docker stop staticweb || true
+docker rm staticweb || true
